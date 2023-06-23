cf0c4257dcedbba1af688a22f92c83fde4c7117b
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-06-23 15:11:44 -0700

    Baseline commit

---
+++ send_message_action.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once 'send_message_action.civix.php';
+// phpcs:disable
+use CRM_SendMessageAction_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function send_message_action_civicrm_config(&$config): void {
+  _send_message_action_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function send_message_action_civicrm_install(): void {
+  _send_message_action_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function send_message_action_civicrm_enable(): void {
+  _send_message_action_civix_civicrm_enable();
+}
+
+function send_message_action_civicrm_links(string $op, ?string $objectName, $objectID, array &$links, ?int &$mask, array &$values) {
+  if ($objectName === 'Contribution') {
+    $links[] = [
+      'title' => ts('Send workflow template'),
+      'name' => 'send_workflow',
+      'url' => 'civicrm/task/send_message',
+      'qs' => 'reset=1&id=%%id%%&entity=contribution',
+      'weight' => 200,
+      'is_single_mode' => TRUE,
+      'bit' => 403,
+    ];
+  }
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function send_message_action_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function send_message_action_civicrm_navigationMenu(&$menu): void {
+//  _send_message_action_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _send_message_action_civix_navigationMenu($menu);
+//}
---
+++ templates/CRM/SendMessageAction/Form/Task/SendWorkflowMessage.tpl
@@ -0,0 +1,43 @@
+{if $contact}
+  <div class="crm-submit-buttons">
+      {include file="CRM/common/formButtons.tpl" location="top"}
+  </div>
+  <p>Click send to send  to {$contact.display_name|escape} in their preferred language of
+    <em>{$language|escape}</em> to email address {$email}|escape}</p>
+    {if array_key_exists('template', $form)}
+        {$form.template.label} {$form.template.html}
+    {/if}
+
+  <hr>
+    {if $subject}
+      <h2>Message preview</h2>
+      <div id="thank_you_subject">{$subject}</div>
+      <div id="thank_you_message">{$message}</div>
+    {/if}
+
+    {* FOOTER *}
+  <div class="crm-submit-buttons">
+      {include file="CRM/common/formButtons.tpl" location="bottom"}
+  </div>
+{/if}
+
+{if !$contact}
+  <p>An email cannot be sent because of {$no_go_reason|escape}</p>
+{/if}
+<script type="text/javascript">
+    {literal}
+    CRM.$(function($) {
+      "use strict";
+      CRM.loadPreview = function (preferred_language, contributionID, template) {
+        CRM.api4("WorkflowMessage", "render", {
+          "workflow": template,
+          "language": preferred_language,
+          "values": {"contributionID": contributionID}
+        }).then(function (results) {
+          CRM.$("#thank_you_subject").text(results[0]["subject"]);
+          CRM.$("#thank_you_message").html(results[0]["html"]);
+        })
+      }
+    });
+    {/literal}
+</script>
